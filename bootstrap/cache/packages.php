ed9f253f7a27998a051088deb0e710f81abaa175
Daniel Nieto-González <36118634+nietodaniel@users.noreply.github.com>
2019-09-13 02:29:39 -0500

    Update Htdocs, documentación, despliegue de carpetas

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,120 @@
+<?php return array (
+  'appzcoder/crud-generator' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Appzcoder\\CrudGenerator\\CrudGeneratorServiceProvider',
+    ),
+  ),
+  'appzcoder/laravel-admin' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Appzcoder\\LaravelAdmin\\LaravelAdminServiceProvider',
+    ),
+  ),
+  'barryvdh/laravel-ide-helper' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
+    ),
+  ),
+  'beyondcode/laravel-dump-server' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BeyondCode\\DumpServer\\DumpServerServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'graham-campbell/throttle' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'GrahamCampbell\\Throttle\\ThrottleServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'pragmarx/yaml' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'PragmaRX\\Yaml\\Package\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Yaml' => 'PragmaRX\\Yaml\\Package\\Facade',
+    ),
+  ),
+  'rap2hpoutre/laravel-log-viewer' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Rap2hpoutre\\LaravelLogViewer\\LaravelLogViewerServiceProvider',
+    ),
+  ),
+  'spatie/laravel-activitylog' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Activitylog\\ActivitylogServiceProvider',
+    ),
+  ),
+  'spatie/laravel-analytics' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Analytics\\AnalyticsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Analytics' => 'Spatie\\Analytics\\AnalyticsFacade',
+    ),
+  ),
+  'thepinecode/i18n' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Pine\\I18n\\I18nServiceProvider',
+    ),
+  ),
+);
